1d29c8869552c1e5f030f2dad03ff6882a595e19
MatijaPerkovic1 <matijaperkovich@gmail.com>
2019-07-11 11:34:42 +0200

    add booking functionality

--- src/flight.js
+++ src/flight.js
@@ -6,6 +6,15 @@ function Flight(props) {
 	const [bookedSeats, setBookedSeats] = useState(props.flight.no_of_booked_seats);
 	const [currentPrice, setCurrentPrice] = useState(props.flight.current_price);
 
+	const [booked, setBooked] = useState(localStorage.getItem('flight_id'));
+
+	function handleBookClick () {
+		setBookedSeats(prev => prev + 1);
+		setCurrentPrice(prev => prev + 1);
+		localStorage.setItem('flight_id', props.flight.id);
+		setBooked(props.flight.id);
+	}
+
 	return(
 	<div>
 		<h3>{props.flight.name}</h3>
@@ -20,7 +29,7 @@ function Flight(props) {
 		})()}</p>
 		<p>${currentPrice}</p>
 		<p>Seats left: {props.flight.no_of_seats- bookedSeats}</p>
-		{<button>Book!</button>}
+		{(booked != props.flight.id) && <button onClick={handleBookClick}>Book!</button>}
 	</div>
 	)
 }
