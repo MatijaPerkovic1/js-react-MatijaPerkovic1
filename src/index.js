88bead1e2cb7476406ff1159cd488840030fa5a5
MatijaPerkovic1 <matijaperkovich@gmail.com>
2019-07-11 11:26:50 +0200

    display flights

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Flight from './flight.js';
 
-function HelloWorld() {
-  return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-  );
+function FlightsApp() {
+
+	async function getAllFlights() {
+		const response = await fetch('https://flighter-hw7.herokuapp.com/api/flights', {
+        	headers: {
+	        	'Authorization': 'FoQZftPkvXppHhf3tbTpHr6Z',
+	        	'Accept': 'application/json',
+	            'Content-Type': 'application/json'  
+        	}
+        });
+		const data = response.json();
+		return data;
+	};
+
+	const [flights, setFlights] = useState([]);
+	const [displayedFlights, setDisplayedFlights] = useState([]);
+
+	useEffect(() => {
+		getAllFlights().then(data => {
+			setFlights(data.flights);
+			setDisplayedFlights(data.flights);
+			});
+	}, []);
+
+	const allFlights = displayedFlights.map((flight) => <Flight key={flight.id} flight={flight} />);
+
+	return (
+	<div>
+
+		{allFlights}
+		
+	</div>
+	);
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<FlightsApp />, document.getElementById('root'));
