299f5c6ed2b46705a3687b763e297b0ef8ed34ab
MatijaPerkovic1 <matijaperkovich@gmail.com>
2019-07-11 20:53:36 +0200

    fix filter

--- src/index.js
+++ src/index.js
@@ -17,41 +17,22 @@ function FlightsApp() {
 	};
 
 	const [flights, setFlights] = useState([]);
-	const [displayedFlights, setDisplayedFlights] = useState([]);
+	const [filter, setFilter] = useState("");
 
 	useEffect(() => {
 		getAllFlights().then(data => {
 			setFlights(data.flights);
-			setDisplayedFlights(data.flights);
 			});
 	}, []);
 
-	function filterFlights(flight, option, filterText) {
-		const flightArrivalAndDeparture = flight.name.split('-');
-		if(option === 1) { //filter by arrival
-			if(flightArrivalAndDeparture[1].includes(filterText)) return flight;
-		} else { //filter by departure
-			if(flightArrivalAndDeparture[0].includes(filterText)) return flight;
-		}
+	function onInputChange(e) {
+		setFilter(e.target.value);
 	}
-
-
-	function onArrivalInputChange(e) {
-		setDisplayedFlights(displayedFlights.filter((flight) => filterFlights(flight, 1, e.target.value)));
-	}
-
-	function onDepartureInputChange(e){
-		setDisplayedFlights(displayedFlights.filter((flight) => filterFlights(flight, 0, e.target.value)));
-	}
-
-
-	const allFlights = displayedFlights.map((flight) => <Flight key={flight.id} flight={flight} />);
 
 	return (
 	<div>
-		Search by arrival: <input onChange={onArrivalInputChange} type="text" name="arrival"/> <br />
-		<br/>Search by departure: <input onChange={onDepartureInputChange} type="text" name="departure" on/>
-		{allFlights}
+		Search: <input onChange={onInputChange} value={filter} type="text" name="arrival"/> <br />
+		{flights.filter((flight) => flight.name.includes(filter)).map((flight) => <Flight key={flight.id} flight={flight} />)}
 	</div>
 	);
 }
