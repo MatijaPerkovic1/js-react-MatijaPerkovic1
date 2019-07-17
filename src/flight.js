afe5b5a7bbfc5a3d95a0e55735252decfd9afd92
MatijaPerkovic1 <matijaperkovich@gmail.com>
2019-07-17 17:21:42 +0200

    correct homework

--- src/flight.js
+++ src/flight.js
@@ -2,10 +2,6 @@ import React, { useState} from 'react';
 import { useLocalStorage } from 'react-use';
 
 function Flight(props) {
-
-	const [bookedSeats, setBookedSeats] = useState(props.flight.no_of_booked_seats);
-	const [currentPrice, setCurrentPrice] = useState(props.flight.current_price);
-
 	return(
 	<div className="item">
 		<img src="https://news.gtp.gr/wp-content/uploads/2019/04/Croatia-Airlines.jpg" />
@@ -15,13 +11,13 @@ function Flight(props) {
 
 			return `${flysAtDate.getHours()}:${flysAtDate.getMinutes()}`;
 		})()}</b></p>
-		<p>{props.flight.company_name}</p>
+		<p>{props.flight.name}</p>
 		<div>
 			<span className="fa fa-star checked"></span>
 			<span className="fa fa-star checked"></span>
 			<span className="fa fa-star checked"></span>
 		</div>
-		<pre id="tickets">|  {props.flight.no_of_seats- bookedSeats} tickets available</pre>
+		<pre id="tickets">|  {props.flight.no_of_seats} tickets available</pre>
 		<pre>Price  <span id="price">{props.flight.current_price}$</span></pre>
 		</div>
 	</div>
--- src/flightsapp.js
+++ src/flightsapp.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useAsync } from 'react-use';
 import { Link } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import Flight from './flight.js';
@@ -6,26 +7,20 @@ import './landing.css';
 
 function FlightsApp() {
 
-	async function getAllFlights() {
-		const response = await fetch('https://flighter-hw7.herokuapp.com/api/flights', {
+	function getAllFlights() {
+		return fetch('https://flighter-hw7.herokuapp.com/api/flights', {
         	headers: {
 	        	'Authorization': 'FoQZftPkvXppHhf3tbTpHr6Z',
 	        	'Accept': 'application/json',
 	            'Content-Type': 'application/json'  
         	}
-        });
-		const data = response.json();
-		return data;
+        })
+        .then((response) => response.json())
+        .then((response) => response.flights);
 	};
 
-	const [flights, setFlights] = useState([]);
 	const [filter, setFilter] = useState("");
-
-	useEffect(() => {
-		getAllFlights().then(data => {
-			setFlights(data.flights);
-			});
-	}, []);
+	const { value: flights } = useAsync(getAllFlights);
 
 	function onInputChange(e) {
 		setFilter(e.target.value);
@@ -57,7 +52,7 @@ function FlightsApp() {
 	</div>
 	<p id="results">RESULTS</p>
 	<div className="wrapper">
-		{flights.filter((flight) => flight.name.includes(filter)).map((flight) => <Flight key={flight.id} flight={flight} />)}
+		{flights && flights.filter((flight) => flight.name.includes(filter)).map((flight) => <Flight key={flight.id} flight={flight} />)}
 	</div>
 	</div>
 	</div>
